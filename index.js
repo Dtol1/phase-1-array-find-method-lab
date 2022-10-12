5d0e60e1a9b560a9294b5c6afb35c3cac0830370
Davis Toler <tolerdavis0@gmail.com>
2022-10-12 09:41:14 -0500

    commit tree 7eec69eec4f322520c0788dcc01546da1694d6d1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+// function superbowlWin(record){
+//     let findW = record.find(superbowlWin)
+//     return record.result === "W" 
+//   }
+  
+//   let findW = record.find(superbowlWin)
+
+function superbowlWin(record){
+    let findW = record.find(win => win.result ==="W")
+    if (findW){
+        return findW.year
+    }
+    return findW
+}
+
+
